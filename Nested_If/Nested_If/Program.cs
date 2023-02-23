67b2b52a74ce1fb627bffae763b5862c6178bd2e
DivyaShitole16 <114485356+DivyaShitole16@users.noreply.github.com>
2023-02-23 21:39:45 +0530

    Nested if

---
+++ Nested_If/Nested_If/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nested_If
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 20;
+            if (i == 20)
+            {
+                if (i > 60)
+                {
+                    Console.WriteLine("i is smaller than 10");
+                }
+                else
+                {
+                    Console.WriteLine("i is greater than 50");
+                }
+            }
+            Console.ReadKey();
+        
+
+        }
+    }
+}
