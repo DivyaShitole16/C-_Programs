640b300e1bcfaaabb160aad17971c93d2a117ffc
DivyaShitole16 <114485356+DivyaShitole16@users.noreply.github.com>
2023-03-31 20:56:19 +0530

    Add files via upload

---
+++ One_dimensional/One_dimensional/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace One_dimensional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] weekDays;
+            weekDays = new string[] { "Sunday","Monday","Tuesday","Wednesday",     
+          "Thursday", "Friday", "satursday" };
+            foreach (string day in weekDays)
+                Console.WriteLine(day + " ");
+            Console.ReadKey();
+
+        }
+    }
+}
